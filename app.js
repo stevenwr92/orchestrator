447a82bc02e8b851639e1d2e75835e6db9027532
stevenwr92 <stevenwr92@gmail.com>
2022-10-04 14:36:37 +0700

    redeploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require("apollo-server");
-const port = 4000;
+const port = process.env.PORT || 4000;
 const { bookTypeDefs, bookResolvers } = require("./schema/book");
 const { postTypeDefs, postResolvers } = require("./schema/post");
 const { userTypeDefs, userResolvers } = require("./schema/user");
